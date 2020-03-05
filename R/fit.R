8c3ed97c92f8337e8bf645ae19e5d21ac9c01a9f
saurav99 <saurav13iitr@gmail.com>
2020-03-05 14:25:18 -0800

    added part of fit function

--- R/fit.R
+++ R/fit.R
@@ -1,3 +1,52 @@
+compute_distance <- function(samples, centers){
+  
+#  samples_trans <- as.data.frame(t(samples))
+  
+  k <- dim(centers)[1]
+  num_features <- dim(centers)[2]
+  n_samples <- dim(samples)[1]
+  
+  distance <- data.frame(matrix(0, nrow = n_samples, ncol = k))
+  center_diff <- data.frame(matrix(0, nrow = n_samples, ncol = num_features))
+  
+  for (i in (1:k)){
+    
+    for (j in (1:n_samples)){
+      
+      center_diff[j,] <- samples[j,] - centers[i,]
+    }
+    
+    center_diff <- data.frame(center_diff^2)
+    
+    sum_center_diff_sq <- apply(center_diff, 2, FUN=sum)
+    
+    sum_center_diff_sq <- data.frame(sum_center_diff_sq^0.5)
+    
+    distance[, i] = sum_center_diff_sq
+    
+  }
+  
+  return(distance)
+  
+}
+
+random_init <- function(samples, k, n_features){
+  
+  centers = data.frame(matrix(0, nrow = k, ncol = n_features))
+  
+  min_feature <- apply(samples, 2, FUN=min)
+  
+  max_feature <- apply(samples, 2, FUN=max)
+  
+  for (i in (1:k))
+  
+    for (j in (1:dim(samples)[1])){
+     
+      centers[i,j] <- sample(samples[,j], 1) 
+    }
+    
+}
+
 #' This takes non-labeled data to perform unsupervised classification
 #' using kmeans algorithm into k clusters.
 #' It returns labels for each data point according to the cluster it belong and also cluster centers
@@ -17,5 +66,7 @@
 #' kmeans['labels']
 
 fit <- function(X_train, k) {
+  
+  
 
 }
