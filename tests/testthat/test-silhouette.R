f6a803567aed8920c0f57f3a958703c13166399d
James Huang <jamesh4@alumni.ubc.ca>
2020-03-14 14:59:52 -0700

    Update tests/testthat/test-silhouette.R
    Co-Authored-By: Saurav Chowdhury <saurav13iitr@gmail.com>

--- tests/testthat/test-silhouette.R
+++ tests/testthat/test-silhouette.R
@@ -1,5 +1,5 @@
 library(testthat)
-
+# Helper data
 X <- data.frame(x1 = c(1, 2, 3, 10, 11, 12), x2 = c(1, 2, 3, 10, 11, 12))
 X2 <- data.frame(x1 = c(1, 2, 3, 10, 11, 12), x2 = c(1, 2, 3, "sdafda", 11, 12))
 labels1 <- c(1,1,1,1,1,2)
