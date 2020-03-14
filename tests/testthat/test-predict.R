77b5a242cd632040055ebe6121e49f9a692a6ba4
Rob Blumberg <robertblumberg97@gmail.com>
2020-03-13 22:18:10 -0700

    added test for new exceptions

--- tests/testthat/test-predict.R
+++ tests/testthat/test-predict.R
@@ -2,17 +2,18 @@ library(testthat)
 
 tests <- function() {
   #helper data
-  data <- data.frame("x1" = c(1, 4, 3, 5, 1, 6, 1, 4, 15, 14, 6, 3),
-                     "x2" = c(1, 2, 3, 5, 1, 6, 8, 4, 15, 2,  6, 3),
-                     "x3" = c(1, 2, 3, 5, 7, 6, 3, 4, 15, 2,  6, 3))
   data_new <- data.frame("x1" = c(1, 1,  15, 1,  3),
                          "x2" = c(2, 6,  2,  10, 10),
                          "x3" = c(3, 10, 6,  11, 11))
   data_new2 <- data.frame("x1" = c(1, 1,  15, 1,  3, 10, 11, 4, 2),
                           "x2" = c(2, 6,  2,  10, 10, 1, 43, 4, 1),
                           "x3" = c(3, 10, 6,  11, 11, 3, 10, 3, 20))
+  data_new3 <- data.frame("x1" = c(1, 1,  15, 1,  3),
+                         "x2" = c(2, 6,  2,  10, 10),
+                         "x3" = c("a", 10, 6,  11, 11))
   centroids <- list(c(1,2,3), c(1,6,10), c(15,2,6), c(1,10,11), c(3,10,11))
   centroids2 <- list(c(1,2,3), c(1,6,10), c(15,2,6), c(1,10,11), c(3,10,11,12))
+  centroids3 <- list(c("a",2,3), c(1,6,10), c(15,2,6), c(1,10,11), c(3,10,11))
   
   #tests
   test_that("Each points in new data needs to be assigned a centroid", {
@@ -29,6 +30,14 @@ tests <- function() {
   
   test_that("Dimensions of centroids must be the same as dimension of data", {
     expect_error(predict(data_new, centroids2))
+  })
+  
+  test_that("Data must be numeric", {
+    expect_error(predict(data_new3, centroids))
+  })
+  
+  test_that("Centroid coordinates must be numeric", {
+    expect_error(predict(data_new, centroids3))
   })
   
 }
